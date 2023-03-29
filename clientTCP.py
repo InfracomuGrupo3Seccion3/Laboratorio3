1b40c2207f6351a8d83eac64e47288d81375b149
Daniel Bernal <da.bernalc1@uniandes.edu.co>
2023-03-28 23:36:20 -0500

    Intento de TCP

---
+++ clientTCP.py
@@ -0,0 +1,114 @@
+import socket
+import os
+import time
+import threading
+import hashlib
+
+# Dirección IP del servidor
+#IP = '192.168.81.1'
+IP = socket.gethostbyname(socket.gethostname())
+# Puerto del servidor
+PORT = 8000
+
+# Dirección del servidor
+ADDR = (IP, PORT)
+
+# Máximo número de conexiones concurrentes
+MAX_CONNECTIONS = 25
+
+# Tamaño máximo de los paquetes a enviar
+BSIZE = 1024
+
+HASHINCORRECTO = 0
+
+def messageReceiver(clientSocket, fileName, fileSize, idClient, tamClients, f):
+    print(f"[{idClient}] Waiting for message...")
+    confirmation = input(f"Cliente {idClient}: ¿Desea recibir el archivo? (S/N): ")
+    clientSocket.sendall(confirmation.encode('utf-8'))
+    print(f"[{idClient}] Message sent.")
+    print(f"[{idClient}] Waiting for message...")
+    
+    # Verificar el directorio de descargas
+    if not os.path.exists("ReceivedFiles"):
+        os.mkdir("ReceivedFiles")
+        
+    with open(f"ReceivedFiles/{fileName}", "wb") as f:
+        cont = 0
+        while cont < int(fileSize):
+            data = clientSocket.recv(BSIZE)
+            f.write(data)
+            cont += BSIZE
+            print(f"[{idClient}] {cont} bytes received.")
+    
+    clientSocket.sendall("Archivo recibido correctamente.".encode('utf-8'))
+    
+    print(f"[{idClient}] All bytes received.")
+    
+    file = open(f"ReceivedFiles/{fileName}", 'r')
+    hash = clientSocket.recv(BSIZE).decode('utf-8')
+    hashAlgorithm = hashlib.md5(file.read().encode()).hexdigest()
+    
+    print(f"[{idClient}] Hash received: {hash}")
+    print(f"[{idClient}] Hash generated: {hashAlgorithm}")
+    
+    ADDR = clientSocket.getpeername()
+    check = ""
+    
+    if hash == hashAlgorithm:
+        print(f"[{idClient}] Hashes match.")
+        clientSocket.sendall("Hashes match.".encode('utf-8'))
+        check = "OK"
+    else:
+        print(f"[{idClient}] Hashes don't match.")
+        HASHINCORRECTO+=1
+        check = "ERROR"
+        f.write(f"[client][{idClient}][{ADDR}] {fileName} Hash received/n")
+    
+    clientSocket.sendall(check.encode('utf-8'))
+    clientSocket.close()
+    
+    
+def main():
+    # Conexión al servidor
+    clientSockets = []
+    tamClients = int(input("Ingrese el número de clientes: "))
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientSocket.connect(ADDR)
+    print(f"[KING CLIENT] Connected to server at {IP}:{PORT}")
+    clientSocket.sendall(str(tamClients).encode('utf-8'))
+    transmissionFile = input("Ingrese el nombre del archivo a transmitir: ")
+    print(f"[KING CLIENT] se espera el archivo {transmissionFile}")
+    transmissionFile = transmissionFile.encode('utf-8')
+    clientSocket.sendall(transmissionFile)
+    print(f"[KING CLIENT] se envio el nombre del archivo {transmissionFile}")
+    filesize = clientSocket.recv(BSIZE).decode('utf-8')
+    print(f"[KING CLIENT] se recibio el tamaño del archivo {filesize}")
+
+    if not os.path.exists('Logs'):
+        os.makedirs('Logs')
+    f= open('Logs/'+time.strftime("%Y-%m-%d-%H-%M-%S")+'-log.txt', 'w') 
+    f.write(f"Archivo: {transmissionFile} Tamaño: {filesize} bytes\n")
+    f.write(f"Clientes: {tamClients}\n")
+    f.write(f"Tiempo de transferencia: \n")
+
+    for i in range(tamClients):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(ADDR)
+        clientSockets.append(client_socket)
+        print(f"[CLIENT {i}] Connected to server at {IP}:{PORT}")
+
+    # Recepción de mensajes
+    threads = []
+    for i in range(tamClients):
+        id_cliente = i
+        thread = threading.Thread(target=messageReceiver, args=(clientSockets[i],transmissionFile.decode('utf-8'),filesize,id_cliente,tamClients,f))
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        thread.join()
+    f.close()
+    clientSocket.sendall("Archivo recibido correctamente.".encode('utf-8'))
+    clientSocket.close()
+if __name__ == "__main__":
+    main()
+    print(f"[CLIENT] HASH incorrectos: {HASHINCORRECTO}")
---
+++ serverTCP.py
@@ -0,0 +1,130 @@
+import socket
+import os
+import datetime
+import hashlib
+import threading
+import time
+
+# Dirección IP del servidor
+#IP = '192.168.81.1'
+IP = socket.gethostbyname(socket.gethostname())
+
+# Puerto del servidor
+PORT = 8000
+
+# Dirección del servidor
+ADDR = (IP, PORT)
+
+# Máximo número de conexiones concurrentes
+MAX_CONNECTIONS = 25
+
+# Tamaño máximo de los paquetes a enviar
+BSIZE = 1024
+
+# Hash utilizado para verificar la integridad del archivo
+HASH_ALGORITHM = hashlib.sha256()
+
+
+def ClientHandler(idClient, fileName, conn, addr):
+    print(f"[NEW CONNECTION] Client {idClient} connected.")
+    
+    # Recibir el nombre del archivo a enviar
+    ready = conn.recv(BSIZE).decode('utf-8')
+    print(f"[{idClient}] Filename: {ready}")
+    
+    
+    # Abrir el archivo
+    file = open(fileName, 'r')
+    print(f"[file][{addr}] {fileName} opened.")
+    
+    # Obtener el tamaño del archivo
+    tam = os.path.getsize(fileName)
+    
+    
+    # Enviar el archivo por fragmentos de tamaño BSIZE
+    with open(fileName, "rb") as f:
+        cont = 0
+        while cont < tam:
+            data = f.read(BSIZE)
+            conn.sendall(data)
+            cont += BSIZE
+            print(f"[{idClient}] {cont} bytes sent.")
+    
+    
+    # Recibir la confirmación de que el archivo fue recibido correctamente
+    confirmation = conn.recv(BSIZE).decode('utf-8')
+    print(f"[{idClient}] {confirmation}")
+    
+
+    # Hash del archivo
+    hashFile = HASH_ALGORITHM.md5(file.read().encode()).hexdigest()
+    print(f"[file][hash][{addr}] {fileName} hash generated.")
+    
+    # Enviar el hash del archivo
+    conn.sendall(hashFile.encode('utf-8'))
+    print(f"[file][hash][{addr}] {fileName} hash sent.")
+    
+    confirmation = conn.recv(BSIZE).decode('utf-8')
+    
+    if confirmation == "OK":
+        print(f"[file][{addr}] {fileName} sent successfully.")
+    else:
+        f.write(f"[file][{addr}] {fileName} sent unsuccessfully.")
+    
+    
+    # Cerrar la conexión
+    conn.close()
+    
+    
+def main():
+    
+    #now = datetime.datetime.now()
+    
+    print("[STARTING] server is starting...")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
+    server.listen(MAX_CONNECTIONS)
+    print(f"[LISTENING] Server is listening on {IP}:{PORT}")
+    
+    # Recibir la conexión inicial
+    intialConnection, addr = server.accept()
+    print(f"[MASTER CONNECTION] {addr} connected.")
+    
+    # Recibir el número de clientes
+    clientCant = int(intialConnection.recv(BSIZE).decode('utf-8'))
+    print(f"[MASTER CONNECTION] {clientCant} clients connected.")
+    
+    # Recibir el nombre del archivo a enviar
+    transferFile = intialConnection.recv(BSIZE).decode('utf-8')+ ".txt"
+    print(f"[MASTER CONNECTION] {transferFile} file to send.")
+    
+    # Enviar la confirmación de que el archivo fue recibido correctamente
+    intialConnection.sendall("OK".encode('utf-8'))
+    
+    # Crea el directorio de registro si no existe
+    if not os.path.exists("Logs"):
+        os.makedirs("Logs")
+        
+    # Crea el archivo de registro
+    logFileName = open('Logs/'+time.strftime("%Y-%m-%d-%H-%M-%S")+'-log.txt', 'w') 
+    logFileName.write(f"File: {transferFile} \n")
+    logFileName.write(f"Clientes: {clientCant} \n")
+    #logFileName.write(f"Time: {now} \n")
+    
+    
+    # Aceptar conexiones de los clientes
+    for i in range(clientCant):
+        conn, addr = server.accept()
+        thread = threading.Thread(target=ClientHandler, args=(i, transferFile, conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+    
+    
+    # Cerrar el archivo de registro
+    if intialConnection.recv(BSIZE).decode('utf-8') == "DISCONNECT":
+        server.close()
+        print("[SERVER] Server is closed.")
+        logFileName.close()
+    
+if __name__ == "__main__":
+    main()
